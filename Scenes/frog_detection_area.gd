a7263fba7ce9032cdff5de51653d6051ce830e9a
Thomas Mezzie <99389013+NotTom31@users.noreply.github.com>
2024-09-20 01:05:35 -0700

    Removed print statements

--- Scenes/frog_detection_area.gd
+++ Scenes/frog_detection_area.gd
@@ -2,8 +2,6 @@ extends Area2D
 class_name FrogDetectionArea
 
 func _on_area_entered(area: Area2D) -> void:
-	print(area.get_parent().name)
 	var parent = area.get_parent()
 	if area.get_parent() is BasicFrog:
-		print("inif")
 		parent.transition_to_swim()
