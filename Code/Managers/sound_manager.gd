4c5cbcd83d7cff352ae7249ae3eaf2dafb44039c
Stanleykitten <brendan.s.pyle@gmail.com>
2024-10-09 19:57:36 -0700

    Updated tower heights for dynamic music, organized sound manager

--- Code/Managers/sound_manager.gd
+++ Code/Managers/sound_manager.gd
@@ -1,29 +1,33 @@
 class_name SoundManager
 extends Node2D
 
-# this code was made by brendan so enter at ur own risk
-# TOC:
-# Audio Configs
-# Variables/Utils COMPLETE
-# Music STILL NEEDS LAYER SWITCHING LOGIC
-# Bus management
-# SFX management 
+# brendan's beautiful perfect code
+
+# Table of Contents:
+# 1) AUDIO CONFIGS (~16)
+	# audio paths and customizations for db, pitch, etc set
+# 2) VARIABLES/UTILS: (~53)
+	# buses are initialized
+	# randomizer and round robin variable
+# 3) MUSIC: (~75)
+	# ambiance
+	# play music on level start
+	# dynamic music layer system
+	# toggle shop / main
+# 4) SFX: (~162)
+	# take calls from elsewhere in code
+	# check audio configs
+	# round-robin style player
+# 5) BUS MANAGEMENT: (~279)
+	# logic used for volume sliders in settings menu 
 
 
-# AUDIO CONFIGS
-# this portion of code is where audio file paths are specified
-# additionally, the configs where volume, pitch variety, and volume variety are set
-# key to audio_configs[0-5]: 0: dbset, 1: dbmin, 2: dbmax, 3: pitchmin,4: pitchmax, 5: audiopaths (array of string(s))
-	# zeroeth is volume! average db, 0 by default
-	# first is db minimum! most that could be subtracted from db using randmomizer
-	# second is db max! most that can be added to db using randomizer, should be smaller abs value than min
-	# third is pitch min! 0.5 value would mean frequencies halved (lower pitch)
-	# fourth is pitch max! 2 means an octave up
-	# fifth is file name, get by copying path of file
 
+# 1) AUDIO CONFIGS
+# this portion of code is where volume, volume variety, and pitch variety are set for each effect
+# additionally, the configs audio file path(s) are specified
 var sound_config = {
-	#boing has 4 sound path options
-	# key to audio_configs[0-5]: 0: dbset, 1: dbmin, 2: dbmax, 3: pitchmin,4: pitchmax, 5: audiopaths
+	# key located below array
 	"boing1": [-2, -4, 2, 0.7, 1.3, ["res://Assets/Audio/SFX/boing_1_-7dB.wav","res://Assets/Audio/SFX/boing_2_-7dB.wav","res://Assets/Audio/SFX/boing_3_-7dB.wav","res://Assets/Audio/SFX/boing_4_-7dB.wav"]],
 	"bounce_charge": [0, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/bounce_charge1_-7dB.wav", "res://Assets/Audio/SFX/bounce_charge2_-7dB.wav"]],
 	"frog_snap": [-1, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/frogsnap_-7dB.wav"]],
@@ -37,19 +41,28 @@ var sound_config = {
 	"lillypad_impact": [0, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/Newstufftoadd/lilypadimpact_-8dB.wav"]],
 	"beep_text": [-3, -4, 5, 0.7, 1.3, ["res://Assets/Audio/SFX/GWJ73_SFX_beepText_loop_-7dB.wav"]],
 }
+# key to audio_configs[0-5]: 0: dbset, 1: dbmin, 2: dbmax, 3: pitchmin,4: pitchmax, 5: audiopaths (array of string(s))
+	# [0] dbset is volume!                                                
+	# [1] dbmin is most that could be subtracted from db using randomizer!
+	# [2] dbmax is most that could be added to db        using randomizer! (consider log scale: same abs value has larger impact than min)
+	# [3] pitchmin is most pitch would be decreased      using randomizer! ex: 0.5 means frequencies halved (octave down)
+	# [4] pitchmax is most pitch would be increased      using randomizer! ex: 2 means frequencies doubled (octave up)
+	# [5] is array of possible file paths!                                 get by copying path of file
+	# recommended initial settings: [0, -4, 2, 0.85, 1.2, ["path"]]
+	
+# 2) VARIABLES AND UTILS
+var rng = RandomNumberGenerator.new() # usded to determine pitch/db offset, filepath if multiple
+var roundrobin = 1 # used by SFX player, expected value 1-n where n is max sfx players (13 as of 10-9-24)
 
-# VARIABLES AND UTILS
+# establish BUSES
 
-# establish utils
-var rng = RandomNumberGenerator.new()
-var roundrobin = 1 # currently general. may need multiple rrs later
-
-# establish variables for bus management
 # master bus
 var masterbus = AudioServer.get_bus_index("Master")
-# music bus to collect main theme and shop theme
+
+# MUSIC BUSES
+# music bus to collect main theme and shop theme (used by music slider)
 var musicbusall = AudioServer.get_bus_index("MXALL")
-# music bus to collect different layers of main theme
+# music bus to collect layers of main theme (for switching between shop and main)
 var musicbuslayers = AudioServer.get_bus_index("MXLAYERS")
 # separate buses for all layers - 1 being base with 5 being top layer
 var musicbus1 = AudioServer.get_bus_index("MX1")
@@ -60,15 +73,16 @@ var musicbus5 = AudioServer.get_bus_index("MX5")
 # music bus for just shop music
 var shopbus = AudioServer.get_bus_index("MXSHOP")
 
-# sfx bus
+# SFX BUS
 var fxbus = AudioServer.get_bus_index("SFX")
 
-# sfx bus
+# ATM BUS
 var atmbus = AudioServer.get_bus_index("ATM")
 
-# MUSIC
 
-# play all layers at init. immediately mute all but first layer.
+# 3) MUSIC
+
+# setup ambiance
 func _ready() -> void:
 	set_ambiance_on(true)
 
@@ -103,113 +117,61 @@ func play_base_music(on : bool) -> void:
 	AudioServer.set_bus_mute(musicbus5, not AudioServer.is_bus_mute(musicbus5))
 	AudioServer.set_bus_mute(shopbus, not AudioServer.is_bus_mute(shopbus))
 
-# code here for changing layers
-# move to bus management section
-# pseudocode:
-#func musiclayerchange(lay) -> void:
-	# ya i cannot comprehend this right now. only code.
-	# perhaps this should be a variable declared in utils so muting/unmuting can be done with reference to previous state... i think the mute code i have been using is toggle
-
-# BUS MANAGEMENT
+# dynamic music layer system 
 func change_music_layer(layer : int):
-	match layer:
-		0:
+	var musiclayerdivisions = [2,4,6,8] # the first tallest tower height in which the next music layer is played
+	if layer < musiclayerdivisions[0]: # only base layer plays      
 			AudioServer.set_bus_mute(musicbus1, false)
 			AudioServer.set_bus_mute(musicbus2, true)
 			AudioServer.set_bus_mute(musicbus3, true)
 			AudioServer.set_bus_mute(musicbus4, true)
 			AudioServer.set_bus_mute(musicbus5, true)
-		1:
+	elif layer < musiclayerdivisions[1]: # only first two layers play
 			AudioServer.set_bus_mute(musicbus1, false)
 			AudioServer.set_bus_mute(musicbus2, false)
 			AudioServer.set_bus_mute(musicbus3, true)
 			AudioServer.set_bus_mute(musicbus4, true)
-			AudioServer.set_bus_mute(musicbus5, true)
-		2:
+			AudioServer.set_bus_mute(musicbus5, true)	
+	elif layer < musiclayerdivisions[2]: # only first three layers play
 			AudioServer.set_bus_mute(musicbus1, false)
 			AudioServer.set_bus_mute(musicbus2, false)
 			AudioServer.set_bus_mute(musicbus3, false)
 			AudioServer.set_bus_mute(musicbus4, true)
 			AudioServer.set_bus_mute(musicbus5, true)
-		3:
+	elif layer < musiclayerdivisions[3]: # only first four layers play
 			AudioServer.set_bus_mute(musicbus1, false)
 			AudioServer.set_bus_mute(musicbus2, false)
 			AudioServer.set_bus_mute(musicbus3, false)
 			AudioServer.set_bus_mute(musicbus4, false)
 			AudioServer.set_bus_mute(musicbus5, true)
-		4:
+	else: # all layers play
 			AudioServer.set_bus_mute(musicbus1, false)
 			AudioServer.set_bus_mute(musicbus2, false)
 			AudioServer.set_bus_mute(musicbus3, false)
 			AudioServer.set_bus_mute(musicbus4, false)
 			AudioServer.set_bus_mute(musicbus5, false)
-		_:
-			print("Music layer out of bounds")
+	
+	
 
+# toggle shop / main theme
 func shop_music(is_playing : bool):
 	AudioServer.set_bus_mute(shopbus, !is_playing)
 	AudioServer.set_bus_mute(musicbuslayers, is_playing)
 
-
-# master bus manager
-# idk get val from slider? not sure tbh
-func _on_main_slider_mouse_exited() -> void:
-	$AudioManagerControls/HBoxContainer/VBoxContainer/MainSlider.release_focus()
-# change bus to slider val (db rectified slider range is 0-1)
-func _on_main_slider_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(masterbus,linear_to_db(value))
-func _on_main_toggle_pressed() -> void:
-	AudioServer.set_bus_mute(masterbus, not AudioServer.is_bus_mute(masterbus))
-
-# music bus manager
-# note: this is for the UI, modifies main music bus not individual parts
-# idk get val from slider? not sure tbh
-func _on_mx_slider_mouse_exited() -> void:
-	$AudioManagerControls/HBoxContainer/VBoxContainer/MXSlider.release_focus()
-# change bus to slider val (db rectified slider range is 0-1)
-func _on_mx_slider_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(musicbusall,linear_to_db(value))
-# toggle
-func _on_mx_toggle_pressed() -> void:
-	AudioServer.set_bus_mute(musicbusall, not AudioServer.is_bus_mute(musicbusall))
-
-# fx bus manager
-# idk get val from slider? not sure tbh
-func _on_fx_slider_mouse_exited() -> void:
-	$AudioManagerControls/HBoxContainer/VBoxContainer/FXSlider.release_focus()
-# change bus to slider val (db rectified slider range is 0-1)
-func _on_fx_slider_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(fxbus,linear_to_db(value))
-# toggle
-func _on_fx_toggle_pressed() -> void:
-	AudioServer.set_bus_mute(fxbus, not AudioServer.is_bus_mute(fxbus))
-	
-# atm bus manager
-# idk get val from slider? not sure tbh
-func _on_atm_slider_mouse_exited() -> void:
-	$AudioManagerControls/HBoxContainer/VBoxContainer/ATMSlider.release_focus()
-# change bus to slider val (db rectified slider range is 0-1)
-func _on_atm_slider_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(atmbus,linear_to_db(value))
-# toggle
-func _on_atm_toggle_pressed() -> void:
-	AudioServer.set_bus_mute(atmbus, not AudioServer.is_bus_mute(atmbus))
-
-#SFX MANAGER
+# 4) SFX
 
 # intake calls from elsewhere in code
-# match string in dictionary defined at top
+# match string in sound_configs dictionary
 func play_sfx(name : String):
-	# put try here for debugging
-	#call sfx_manager
-	sfx_manager(sound_config[name])
+	# check if called effect is in config list
+	if sound_config.has(name):
+		# sound found
+		# call sfx manager 
+		sfx_manager(sound_config[name])  # Access the value if the key exists
+	else: # sound not found
+		print("sound_manager could not find configs for \"", name, "\".")
 
-# example call: (outdated)
-# func sfx_manager_example() -> void:
-	
-
-	# here is an example of a call with my default values plugged in
-	
+# configure one of 13 rotating players to play called sound 
 func sfx_manager(audio_configs : Array) -> void:
 	# choose from set of paths if possible
 	var audiopath = randi_range(0, audio_configs[5].size()-1)
@@ -311,6 +273,50 @@ func sfx_manager(audio_configs : Array) -> void:
 			#choose player from round robin
 			$PickupSFX13.play()
 			#print("round robin!")
-	
 
-	
+
+# 5) BUS MANAGEMENT
+
+# master bus manager
+#  get val from slider
+func _on_main_slider_mouse_exited() -> void:
+	$AudioManagerControls/HBoxContainer/VBoxContainer/MainSlider.release_focus()
+# change bus to slider val (db rectified slider range is 0-1)
+func _on_main_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(masterbus,linear_to_db(value))
+func _on_main_toggle_pressed() -> void:
+	AudioServer.set_bus_mute(masterbus, not AudioServer.is_bus_mute(masterbus))
+
+# music bus manager
+# note: this is for the UI, modifies main music bus not individual parts
+# get val from slider
+func _on_mx_slider_mouse_exited() -> void:
+	$AudioManagerControls/HBoxContainer/VBoxContainer/MXSlider.release_focus()
+# change bus to slider val (db rectified slider range is 0-1)
+func _on_mx_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(musicbusall,linear_to_db(value))
+# toggle mx
+func _on_mx_toggle_pressed() -> void:
+	AudioServer.set_bus_mute(musicbusall, not AudioServer.is_bus_mute(musicbusall))
+
+# fx bus manager
+# get val from slider
+func _on_fx_slider_mouse_exited() -> void:
+	$AudioManagerControls/HBoxContainer/VBoxContainer/FXSlider.release_focus()
+# change bus to slider val (db rectified slider range is 0-1)
+func _on_fx_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(fxbus,linear_to_db(value))
+# toggle sfx
+func _on_fx_toggle_pressed() -> void:
+	AudioServer.set_bus_mute(fxbus, not AudioServer.is_bus_mute(fxbus))
+
+# atm bus manager
+# get val from slider
+func _on_atm_slider_mouse_exited() -> void:
+	$AudioManagerControls/HBoxContainer/VBoxContainer/ATMSlider.release_focus()
+# change bus to slider val (db rectified slider range is 0-1)
+func _on_atm_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(atmbus,linear_to_db(value))
+# toggle atm
+func _on_atm_toggle_pressed() -> void:
+	AudioServer.set_bus_mute(atmbus, not AudioServer.is_bus_mute(atmbus))
