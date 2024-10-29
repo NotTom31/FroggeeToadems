0a448032b808b52cec326c8f349581197a422ee5
Thomas Mezzie <99389013+NotTom31@users.noreply.github.com>
2024-10-29 14:24:57 -0700

    Shop ui tweak

--- Code/UI/shop_screen.gd
+++ Code/UI/shop_screen.gd
@@ -72,8 +72,8 @@ var tac_talking = false
 
 @onready var name_label = $Panel/MarginContainer2/VBoxContainer/Name
 @onready var dialogue_label = $Panel/MarginContainer2/VBoxContainer/Text
-@onready var next_button = $Panel/MarginContainer2/VBoxContainer/HBoxContainer/NextButton
-@onready var restart_button = $Panel/MarginContainer2/VBoxContainer/HBoxContainer/ResetButton
+@onready var next_button = $Panel/MarginContainer2/VBoxContainer/VBoxContainer/HBoxContainer/NextButton
+@onready var restart_button = $Panel/MarginContainer2/VBoxContainer/VBoxContainer/HBoxContainer/ResetButton
 
 # Function to set requested frogs for the current customer
 func set_requested_frogs(customer_id: int, frogs: Array):
