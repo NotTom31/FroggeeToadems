9402cfb1a3a9a2a8f27b479a4f1d30d354285fca
Stanleykitten <brendan.s.pyle@gmail.com>
2024-10-29 14:23:12 -0700

    Reverted experimental changes

--- Code/FrogLogic/FrogStates/FrogCharge.gd
+++ Code/FrogLogic/FrogStates/FrogCharge.gd
@@ -2,12 +2,9 @@ extends State
 class_name FrogCharge
 
 #copy of variables for old bounce charge implementation
-#@export var min_charge_time := 1.0
-#@export var max_charge_time := 3.0
+@export var min_charge_time := 1.0
+@export var max_charge_time := 3.0
 
-@export var slow_charge := 5.0
-@export var medium_charge := 3.65
-@export var fast_charge := 2.3
 
 var CanJump
 
@@ -18,41 +15,13 @@ func Enter():
 func Exit():
 	CanJump = false
 
-
+# copy of old implementation
 func play_charge_animation():
 	$"../../FrogSpriteHandler".play_charge()
-	var jump_time = randi_range(0,2) # 0 slow, 1 medium, 2 fast
-	match jump_time:
-		0:
-			var charge_time = slow_charge
-			if get_tree().root.get_child(0) is Main:
-				get_tree().root.get_child(0).play_sound("wert_slow")
-				get_tree().root.get_child(0).play_sound("bounce_charge_slow")
-			await get_tree().create_timer(charge_time).timeout
-		1:
-			var charge_time = medium_charge
-			if get_tree().root.get_child(0) is Main:
-				get_tree().root.get_child(0).play_sound("wert_medium")
-				get_tree().root.get_child(0).play_sound("bounce_charge_medium")
-			await get_tree().create_timer(charge_time).timeout
-		2:
-			var charge_time = fast_charge
-			if get_tree().root.get_child(0) is Main:
-				get_tree().root.get_child(0).play_sound("wert_fast")
-				get_tree().root.get_child(0).play_sound("bounce_charge_fast")
-			await get_tree().create_timer(charge_time).timeout
-
-	
+	if get_tree().root.get_child(0) is Main:
+		get_tree().root.get_child(0).play_sound("wert")
+		get_tree().root.get_child(0).play_sound("bounce_charge_medium")
+	var charge_time = randf_range(min_charge_time, max_charge_time)
+	await get_tree().create_timer(charge_time).timeout
 	if CanJump:
 		Transitioned.emit(self, "FrogJump")
-
-# copy of old implementation
-#func play_charge_animation():
-	#$"../../FrogSpriteHandler".play_charge()
-	#if get_tree().root.get_child(0) is Main:
-		#get_tree().root.get_child(0).play_sound("wert")
-		#get_tree().root.get_child(0).play_sound("bounce_charge")
-	#var charge_time = randf_range(min_charge_time, max_charge_time)
-	#await get_tree().create_timer(charge_time).timeout
-	#if CanJump:
-		#Transitioned.emit(self, "FrogJump")
--- Code/Managers/sound_manager.gd
+++ Code/Managers/sound_manager.gd
@@ -44,26 +44,27 @@ var sound_config = {
 	"close_shop": [0, -4, 2, 0.85, 1.2, ["null"]],
 	# FROG SFX
 	"boing1": [-2, -4, 2, 0.7, 1.3, ["res://Assets/Audio/SFX/boing_1_-7dB.wav","res://Assets/Audio/SFX/boing_2_-7dB.wav","res://Assets/Audio/SFX/boing_3_-7dB.wav","res://Assets/Audio/SFX/boing_4_-7dB.wav"]],
-	"bounce_charge_slow": [-2, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/post-jam addition/bouncecharge3.wav"]],
-	"bounce_charge_medium": [0, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/post-jam addition/bouncecharge3faster.wav"]],
-	"bounce_charge_fast": [1, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/post-jam addition/bouncecharge3fastest.wav"]],
+	"bounce_charge_slow": [-4, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/post-jam addition/bouncecharge3.wav"]],
+	"bounce_charge_medium": [-2, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/post-jam addition/bouncecharge3faster.wav"]],
+	"bounce_charge_fast": [-2, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/post-jam addition/bouncecharge3fastest.wav"]],
 	"wert_slow": [-4, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/post-jam addition/wert.wav"]],
 	"wert_medium": [-5, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/post-jam addition/wertmedium.wav"]],
 	"wert_fast": [-1, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/post-jam addition/wertfastest.wav"]],
+	"wert": [-4, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/post-jam addition/wert.wav", "res://Assets/Audio/SFX/post-jam addition/wertmedium.wav", "res://Assets/Audio/SFX/post-jam addition/wertfastest.wav"]],
 	# unimplemented sound "frog_click" for when clicking on frogs
 	"frog_click": [0, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/frogimpact1.wav","res://Assets/Audio/SFX/frogimpact2.wav"]],
 	# planned sound "frog_spawn" for when a new frog appears
 	"frog_spawn": [0, -4, 2, 0.85, 1.2, ["null"]],
 	"frog_snap": [-1, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/post-jam addition/frogsnap1.wav", "res://Assets/Audio/SFX/post-jam addition/frogsnap2.wav", "res://Assets/Audio/SFX/post-jam addition/frogsnap3.wav"]],
 	"tiny_ribbit" : [-15, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/ribbit_tiny1_-9dB.wav","res://Assets/Audio/SFX/ribbit_tiny2_-9dB.wav","res://Assets/Audio/SFX/ribbit_tiny3_-9dB.wav"]],
-	"default_ribbit": [-3, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/post-jam addition/ribbit_normal.wav"]],
-	"tropical_ribbit": [-3, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/post-jam addition/ribbit_tropical.wav"]],
+	"default_ribbit": [-1, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/post-jam addition/ribbit_normal.wav"]],
+	"tropical_ribbit": [-1, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/post-jam addition/ribbit_tropical.wav"]],
 	"big_ribbit": [-8, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/ribbitBIG_-8dB.wav"]],
 	# MAGIC SFX
-	"magic" : [-4, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/post-jam addition/magic1.wav", "res://Assets/Audio/SFX/post-jam addition/magic2.wav", "res://Assets/Audio/SFX/post-jam addition/magic3.wav"]],
-	"magic_equip" : [-15, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/post-jam addition/magicequip.wav", "res://Assets/Audio/SFX/post-jam addition/magicequip2.wav"]],
-	"magic_unequip" : [-15, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/post-jam addition/magicunequip.wav"]],
-	"magic_fail" : [-4, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/post-jam addition/magicfail1.wav", "res://Assets/Audio/SFX/post-jam addition/magicfail2.wav"]],
+	"magic" : [-3, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/post-jam addition/magic1.wav", "res://Assets/Audio/SFX/post-jam addition/magic2.wav", "res://Assets/Audio/SFX/post-jam addition/magic3.wav"]],
+	"magic_equip" : [-10, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/post-jam addition/magicequip.wav", "res://Assets/Audio/SFX/post-jam addition/magicequip2.wav"]],
+	"magic_unequip" : [-10, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/post-jam addition/magicunequip.wav"]],
+	"magic_fail" : [1, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/post-jam addition/magicfail1.wav", "res://Assets/Audio/SFX/post-jam addition/magicfail2.wav"]],
 	# NPCS
 	"beep_text": [-3, -4, 5, 0.7, 1.3, ["res://Assets/Audio/SFX/GWJ73_SFX_beepText_loop_-7dB.wav"]],
 	# unimplemented mock-up sound "beep_text_gorf" specifically for gorf's ribbit-meows
