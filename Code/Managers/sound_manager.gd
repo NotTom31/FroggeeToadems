af27c754591c6f7b4220b9859410b6de3c533333
Stanleykitten <brendan.s.pyle@gmail.com>
2024-10-29 15:14:16 -0700

    audio rebalance

--- Code/Managers/sound_manager.gd
+++ Code/Managers/sound_manager.gd
@@ -44,13 +44,13 @@ var sound_config = {
 	"close_shop": [0, -4, 2, 0.85, 1.2, ["null"]],
 	# FROG SFX
 	"boing1": [-2, -4, 2, 0.7, 1.3, ["res://Assets/Audio/SFX/boing_1_-7dB.wav","res://Assets/Audio/SFX/boing_2_-7dB.wav","res://Assets/Audio/SFX/boing_3_-7dB.wav","res://Assets/Audio/SFX/boing_4_-7dB.wav"]],
-	"bounce_charge_slow": [-4, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/post-jam addition/bouncecharge3.wav"]],
-	"bounce_charge_medium": [-2, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/post-jam addition/bouncecharge3faster.wav"]],
-	"bounce_charge_fast": [-2, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/post-jam addition/bouncecharge3fastest.wav"]],
-	"wert_slow": [-4, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/post-jam addition/wert.wav"]],
-	"wert_medium": [-5, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/post-jam addition/wertmedium.wav"]],
-	"wert_fast": [-1, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/post-jam addition/wertfastest.wav"]],
-	"wert": [-4, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/post-jam addition/wert.wav", "res://Assets/Audio/SFX/post-jam addition/wertmedium.wav", "res://Assets/Audio/SFX/post-jam addition/wertfastest.wav"]],
+	"bounce_charge_slow": [1, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/post-jam addition/bouncecharge3.wav"]],
+	"bounce_charge_medium": [1, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/post-jam addition/bouncecharge3faster.wav"]],
+	"bounce_charge_fast": [2, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/post-jam addition/bouncecharge3fastest.wav"]],
+	"wert_slow": [0, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/post-jam addition/wert.wav"]],
+	"wert_medium": [0, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/post-jam addition/wertmedium.wav"]],
+	"wert_fast": [2, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/post-jam addition/wertfastest.wav"]],
+	"wert": [0, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/post-jam addition/wert.wav", "res://Assets/Audio/SFX/post-jam addition/wertmedium.wav", "res://Assets/Audio/SFX/post-jam addition/wertfastest.wav"]],
 	# unimplemented sound "frog_click" for when clicking on frogs
 	"frog_click": [0, -4, 2, 0.85, 1.2, ["res://Assets/Audio/SFX/frogimpact1.wav","res://Assets/Audio/SFX/frogimpact2.wav"]],
 	# planned sound "frog_spawn" for when a new frog appears
