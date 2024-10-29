846d7a533f324ea7d6268efffc7f298b0be82fc8
Stanleykitten <brendan.s.pyle@gmail.com>
2024-10-29 14:05:40 -0700

    Experimental bounce charge implementation

--- Code/FrogLogic/FrogStates/FrogCharge.gd
+++ Code/FrogLogic/FrogStates/FrogCharge.gd
@@ -1,10 +1,14 @@
 extends State
 class_name FrogCharge
 
-#copy of variables for old bounce charge implementation
-#@export var min_charge_time := 1.0
-#@export var max_charge_time := 3.0
+# set to false for old implementation, true for brendan's implementation
+@export var toggle_experimental_bounce = true
 
+#normal implementation variables
+@export var min_charge_time := 1.0
+@export var max_charge_time := 3.0
+
+#experimental implementation variables 
 @export var slow_charge := 5.0
 @export var medium_charge := 3.65
 @export var fast_charge := 2.3
@@ -20,32 +24,40 @@ func Exit():
 
 
 func play_charge_animation():
-	$"../../FrogSpriteHandler".play_charge()
-	var jump_time = randi_range(0,2) # 0 slow, 1 medium, 2 fast
-	match jump_time:
-		0:
-			var charge_time = slow_charge
-			if get_tree().root.get_child(0) is Main:
-				get_tree().root.get_child(0).play_sound("wert_slow")
-				get_tree().root.get_child(0).play_sound("bounce_charge_slow")
-			await get_tree().create_timer(charge_time).timeout
-		1:
-			var charge_time = medium_charge
-			if get_tree().root.get_child(0) is Main:
-				get_tree().root.get_child(0).play_sound("wert_medium")
-				get_tree().root.get_child(0).play_sound("bounce_charge_medium")
-			await get_tree().create_timer(charge_time).timeout
-		2:
-			var charge_time = fast_charge
-			if get_tree().root.get_child(0) is Main:
-				get_tree().root.get_child(0).play_sound("wert_fast")
-				get_tree().root.get_child(0).play_sound("bounce_charge_fast")
-			await get_tree().create_timer(charge_time).timeout
-
-	
-	if CanJump:
-		Transitioned.emit(self, "FrogJump")
-
+	if toggle_experimental_bounce == true:
+		$"../../FrogSpriteHandler".play_charge()
+		var jump_time = randi_range(0,2) # 0 slow, 1 medium, 2 fast
+		match jump_time:
+			0:
+				var charge_time = slow_charge
+				if get_tree().root.get_child(0) is Main:
+					get_tree().root.get_child(0).play_sound("wert_slow")
+					get_tree().root.get_child(0).play_sound("bounce_charge_slow")
+				await get_tree().create_timer(charge_time).timeout
+			1:
+				var charge_time = medium_charge
+				if get_tree().root.get_child(0) is Main:
+					get_tree().root.get_child(0).play_sound("wert_medium")
+					get_tree().root.get_child(0).play_sound("bounce_charge_medium")
+				await get_tree().create_timer(charge_time).timeout
+			2:
+				var charge_time = fast_charge
+				if get_tree().root.get_child(0) is Main:
+					get_tree().root.get_child(0).play_sound("wert_fast")
+					get_tree().root.get_child(0).play_sound("bounce_charge_fast")
+				await get_tree().create_timer(charge_time).timeout
+		if CanJump:
+			Transitioned.emit(self, "FrogJump")
+	else:
+		$"../../FrogSpriteHandler".play_charge()
+		if get_tree().root.get_child(0) is Main:
+			get_tree().root.get_child(0).play_sound("wert_medium")
+			get_tree().root.get_child(0).play_sound("bounce_charge_medium")
+		var charge_time = randf_range(min_charge_time, max_charge_time)
+		await get_tree().create_timer(charge_time).timeout
+		if CanJump:
+			Transitioned.emit(self, "FrogJump")
+		
 # copy of old implementation
 #func play_charge_animation():
 	#$"../../FrogSpriteHandler".play_charge()
