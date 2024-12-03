909a182c7a7bd6b11aebba9f058f1eaf1fc97308
Brendan Pyle <brendan.s.pyle@gmail.com>
2024-12-03 15:24:35 -0800

    Please enter the commit message for your changes. Lines starting

--- Code/Legacy/frog_detection_area.gd
+++ Code/Legacy/frog_detection_area.gd
@@ -5,10 +5,10 @@ func _on_area_entered(area: Area2D) -> void:
 	var parent = area.get_parent()
 	if parent is not BasicFrog:
 		return
+	parent.over_water = true
 	var current_state = parent.get_node("StateMachine").current_state
 	if current_state is FrogFall or current_state is FrogJump:
 		parent.transition_to_swim()
-	parent.over_water = true
 
 func _on_area_exited(area: Area2D) -> void:
 	var parent = area.get_parent()
