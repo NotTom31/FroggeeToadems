02ae42e0f826dd74190038ed3c1e473a7d2c0ea6
Brendan Pyle <brendan.s.pyle@gmail.com>
2024-12-06 16:22:55 -0800

    added tower break effect

--- Code/lillypad.gd
+++ Code/lillypad.gd
@@ -19,7 +19,22 @@ func get_held_frog_types() -> Array[MagicManager.FrogType]:
 
 func fountain() -> void:
 	if $FrogSlot.inhabitant != null:
+		# NOTE: 90% of this is just for the sound effect, only mechanical part is last line: $FrogSlot.inhabitant.fountain()
+		# play fountain sounds
+		var snap_count =  float(self.count_frogs_in_stack())
+		var snap_time = .4+.025*snap_count
+		var snap_step = snap_time/snap_count
+		var snap_pitch: float
+		var snap_paths = ["res://Assets/Audio/SFX/post-jam addition/frogsnap1.wav","res://Assets/Audio/SFX/post-jam addition/frogsnap2.wav","res://Assets/Audio/SFX/post-jam addition/frogsnap3.wav"]
 		$FrogSlot.inhabitant.fountain()
+		for snap in range(snap_count):
+			$FountainSnapPlayer1.set_stream(load(snap_paths[randi_range(0,2)])) 
+			snap_pitch = 0.5+.2*snap
+			$FountainSnapPlayer1.set_pitch_scale(snap_pitch)
+			$FountainSnapPlayer1.volume_db = 10 + randf_range(-3, 1)
+			#choose player from round robin
+			$FountainSnapPlayer1.play()
+			await get_tree().create_timer(snap_step*randf_range(0.92,1.1)).timeout
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
