db73bc51a98e6618abb8a0aea51ca5c405194389
Brendan Pyle <brendan.s.pyle@gmail.com>
2025-01-28 11:55:44 -0800

    Prototype works, but I'll prob use this as a reference to restart as I messed up the non-basic instantiations of basicfrog, oops

--- Scenes/Frogs/FrogTypes/watermask.gd
+++ Scenes/Frogs/FrogTypes/watermask.gd
@@ -9,13 +9,14 @@ func _ready() -> void:
 
 func water_mask():
 	clip_children = CLIP_CHILDREN_ONLY
-	#self_modulate = Color(1, 1, 1, 1)
+	self_modulate = Color(1, 1, 1, 1)
 
 func water_unmask():
 	clip_children = CLIP_CHILDREN_DISABLED
-	#self_modulate = Color(1, 1, 1, 0) # clear 
+	self_modulate = Color(1, 1, 1, 0) # clear 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var time_in_seconds = Time.get_ticks_msec() / 1000.0
-	self.offset.y = 5 * sin(time_in_seconds * y_bob_speed)
+	self.offset.y = 10 - 5 * sin(time_in_seconds * y_bob_speed)
+	self.offset.x = 10 * sin(time_in_seconds * y_bob_speed * .5)
